8ade20c679c022538de2439ca09459da531e0147
Alejandrorodram <ale1.zul@gmail.com>
2022-03-09 11:09:23 +0000

    first commit

---
+++ GetMiddleCharacter/Kata.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetMiddleCharacter
+{
+    internal class Kata
+    { 
+        public static string GetMiddleCharacter1(string s)
+        {
+            if (s.Length % 2 == 0)
+            {
+                return s.Substring(s.Length / 2 - 1, 2);
+            }
+            else
+            {
+                return s.Substring(s.Length / 2, 1);
+            }
+        }
+        public static string GetMiddleCharacter2(string s)
+        {
+            return s.Length % 2 == 0 ? s.Substring(s.Length / 2 - 1, 2) : s.Substring(s.Length / 2, 1);
+        }
+        public static string GetMiddleCharacter3(string s)
+        {
+            return string.IsNullOrEmpty(s)
+            ? s
+            : s.Substring((s.Length - 1) / 2, 2 - s.Length % 2);
+        }
+
+        public static string GetMiddleCharacter4(string s)
+        {
+            string result = "";
+            char[] sarr = s.ToCharArray();
+
+            if (s.Length % 2 != 0)
+            {
+                result = sarr[((sarr.Length + 1) / 2) - 1].ToString();
+
+            }
+
+            if (s.Length % 2 == 0)
+            {
+                result = sarr[((sarr.Length + 1) / 2) - 1] + sarr[(sarr.Length + 1) / 2].ToString();
+
+            }
+
+            return result;
+        }
+
+    }
+}
---
+++ GetMiddleCharacter/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetMiddleCharacter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.GetMiddleCharacter1("hola"));
+            Console.WriteLine(Kata.GetMiddleCharacter2("testing"));
+            Console.WriteLine(Kata.GetMiddleCharacter3("abecedario"));
+            Console.WriteLine(Kata.GetMiddleCharacter3("abecedario"));
+
+        }
+    }
+}
